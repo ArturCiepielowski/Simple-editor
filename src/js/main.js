597e9d7559945410c3934facaeea85521209df2b
ArturCiepielowski <artciepi@wp.pl>
2019-07-14 17:39:44 +0200

    feat: adding readme

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,24 @@
 import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+
+
+const focusInput = document.querySelector('.textarea--js');
+const buttSave = document.querySelector('.buttons__save--js');
+const buttRet = document.querySelector('.buttons__return--js');
+
+
+
+
+buttSave.addEventListener('click' , (e) =>{
+
+    localStorage.setItem('focusInput' , focusInput.value);
+
+    console.log(focusInput.value)
+});
+
+
+buttRet.addEventListener('click' , (e) =>{
+
+    focusInput.value=localStorage.getItem('focusInput');
+});
